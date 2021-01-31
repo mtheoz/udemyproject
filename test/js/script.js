881188665858688e0b1fb01e57318af8862f7fd1
mtheoz <theozzz@yandex.ru>
2021-01-31 12:25:09 +0800

    practic task

--- test/js/script.js
+++ test/js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-let x = 13140;
+let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '0');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+let mName1 = prompt('Название фильма');
+let mRating1 = prompt('Рейтинг фильма');
+let mName2 = prompt('Название фильма');
+let mRating2 = prompt('Рейтинг фильма');
 
-console.log(x/6);
-console.log(x/6);
+personalMovieDB.movies[mName1] = mRating1;
+personalMovieDB.movies[mName2] = mRating2;
+console.log(personalMovieDB);
